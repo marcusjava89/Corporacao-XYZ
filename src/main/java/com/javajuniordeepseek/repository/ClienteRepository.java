3b0979f5cf5f0061c89e4fb0208b9113f5b5065e
Marcus Martins <marcusjava89@gmail.com>
2025-05-29 22:03:31 -0300

    Adição de endpoints no controller e método na interface.

--- src/main/java/com/javajuniordeepseek/repository/ClienteRepository.java
+++ src/main/java/com/javajuniordeepseek/repository/ClienteRepository.java
@@ -1,9 +1,20 @@
 package com.javajuniordeepseek.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import com.javajuniordeepseek.model.Clientes;
+import com.javajuniordeepseek.model.Cliente;
 
-public interface ClienteRepository extends JpaRepository<Clientes, Long>{
+import jakarta.transaction.Transactional;
 
+@Repository
+@Transactional
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	@Query("select u from Cliente u where upper(trim(u.nome)) like %?1%")
+	List<Cliente> listagemClientes(String nome); 		
+	
 }
