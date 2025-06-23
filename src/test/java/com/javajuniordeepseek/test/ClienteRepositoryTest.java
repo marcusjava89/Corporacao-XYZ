773b3d3e8efaf9f53d4993ec72a6686bd03fdd6d
Marcus Martins <marcusjava89@gmail.com>
2025-06-23 13:51:07 -0300

    Adiciona teste de buscar cliente por e-mail
    Teste buscar e encontra o cliente no banco de dados pelo e-mail, retor-
    nando o cliente que tem aquele e-mail específico. Não temos duplicata
    de e-mails.

--- src/test/java/com/javajuniordeepseek/test/ClienteRepositoryTest.java
+++ src/test/java/com/javajuniordeepseek/test/ClienteRepositoryTest.java
@@ -81,11 +81,46 @@ public class ClienteRepositoryTest {
 
 		List<Cliente> resposta = clienteRepository.buscaNome("joca");
 
-		assertEquals(0, resposta.size());
+		assertEquals(0, resposta.size()); //Deve retorna uma lista vazia
 
 	}
 	
-	
+	@Test
+	public void testarBuscarPorEmail() {
+		
+		System.out.println(" ");
+		clienteRepository.deleteAll();
+
+		Cliente cliente = new Cliente();
+		cliente.setNome("Fulano da Silva");
+		cliente.setEmail("fulano@email.com");
+		cliente.setTelefone("11999999999");
+		cliente.setDataDeCadastro(java.sql.Date.valueOf("2024-06-10"));
+		clienteRepository.save(cliente);
+
+		Cliente cliente2 = new Cliente();
+		cliente2.setNome("Marcus da Silva");
+		cliente2.setEmail("marcusjava89@gmail.com");
+		cliente2.setTelefone("21999625322");
+		cliente2.setDataDeCadastro(java.sql.Date.valueOf("2024-06-10"));
+		clienteRepository.save(cliente2);
+
+		Cliente cliente3 = new Cliente();
+		cliente3.setNome("Jorge Jesus");
+		cliente3.setEmail("jorgejesus@gmail.com");
+		cliente3.setTelefone("2186450456");
+		cliente3.setDataDeCadastro(java.sql.Date.valueOf("2024-06-10"));
+		clienteRepository.save(cliente3);
+		
+		Cliente clienteObtido = clienteRepository.buscaPorEmail(cliente.getEmail());
+		Cliente clienteObtido2 = clienteRepository.buscaPorEmail(cliente2.getEmail());
+		Cliente clienteObtido3 = clienteRepository.buscaPorEmail(cliente3.getEmail());
+		
+		assertEquals("Fulano da Silva", clienteObtido.getNome());
+		assertEquals("Marcus da Silva", clienteObtido2.getNome());
+		assertEquals("Jorge Jesus", clienteObtido3.getNome());
+		
+	}
 
 }
 
