cb2db58025edd44023b69f11d5110e0fddecfa91
Marcus Martins <marcusjava89@gmail.com>
2025-06-23 13:18:17 -0300

    Adiciona dois testes de buscar Nome no Data Base
    Adiconamos o teste da query de buscar uma lista de nomes no banco de
    dados apenas com parte do nome. O Objetivo do teste 1 foi encontrar os
    nomes e retornar a Lista. O teste 2 buscou com partes de nomes que não
    estav no banco de dados para retornar uma lista vazia.

--- src/main/java/com/javajuniordeepseek/repository/ClienteRepository.java
+++ src/main/java/com/javajuniordeepseek/repository/ClienteRepository.java
@@ -6,23 +6,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.javajuniordeepseek.model.Cliente;
-
-import jakarta.transaction.Transactional;
 
 @Repository
 @Transactional
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 	
-	//método antigo não buscou uma lista de nomes com parte do nome dado
-	@Query("SELECT u FROM Cliente u WHERE UPPER(u.nome) "
-			+ "LIKE UPPER(CONCAT('%', TRIM(:nome), '%'))")
-	List<Cliente> buscaNome(@Param("nome") String nome);
+	@Query("SELECT u FROM Cliente u WHERE UPPER(u.nome) LIKE UPPER(CONCAT('%', TRIM(:nome), '%'))")
+	public List<Cliente> buscaNome(@Param("nome") String nome);
 	
 	@Query("select u from Cliente u where trim(u.email) = ?1 ")
-	Cliente buscaPorEmail(String email);
+	public Cliente buscaPorEmail(String email);
 	
 	@Query("select a.email from Cliente a")
-	List<String> listaEmail();
+	public List<String> listaEmail();
 }
--- src/test/java/com/javajuniordeepseek/test/ClienteRepositoryTest.java
+++ src/test/java/com/javajuniordeepseek/test/ClienteRepositoryTest.java
@@ -1,20 +1,85 @@
 package com.javajuniordeepseek.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.javajuniordeepseek.application.JavaJuniorDeepSeekApplication;
+import com.javajuniordeepseek.model.Cliente;
 import com.javajuniordeepseek.repository.ClienteRepository;
 
-@DataJpaTest
+@SpringBootTest(classes = JavaJuniorDeepSeekApplication.class)
 public class ClienteRepositoryTest {
-	
+
 	@Autowired
-	ClienteRepository clienteRepository;
-	
-	
+	private ClienteRepository clienteRepository;
+
 	@Test
-	public void testarbuscaNome() {
-		
+	public void testarBuscaNomeEncontrado() {
+
+		clienteRepository.deleteAll();
+
+		Cliente cliente = new Cliente();
+		cliente.setNome("Fulano da Silva");
+		cliente.setEmail("fulano@email.com");
+		cliente.setTelefone("11999999999");
+		cliente.setDataDeCadastro(java.sql.Date.valueOf("2024-06-10"));
+		clienteRepository.save(cliente);
+
+		Cliente cliente2 = new Cliente();
+		cliente2.setNome("Marcus da Silva");
+		cliente2.setEmail("marcusjava89@gmail.com");
+		cliente2.setTelefone("21999625322");
+		cliente2.setDataDeCadastro(java.sql.Date.valueOf("2024-06-10"));
+		clienteRepository.save(cliente2);
+
+		Cliente cliente3 = new Cliente();
+		cliente3.setNome("Jorge Jesus");
+		cliente3.setEmail("jorgejesus@gmail.com");
+		cliente3.setTelefone("2186450456");
+		cliente3.setDataDeCadastro(java.sql.Date.valueOf("2024-06-10"));
+		clienteRepository.save(cliente3);
+
+		List<Cliente> resposta = clienteRepository.buscaNome("silva");
+
+		assertEquals(2, resposta.size());
+
 	}
+
+	@Test
+	public void testarNomeBuscaNaoEncontrado() {
+
+		clienteRepository.deleteAll();
+
+		Cliente cliente = new Cliente();
+		cliente.setNome("Fulano da Silva");
+		cliente.setEmail("fulano@email.com");
+		cliente.setTelefone("11999999999");
+		cliente.setDataDeCadastro(java.sql.Date.valueOf("2024-06-10"));
+		clienteRepository.save(cliente);
+
+		Cliente cliente2 = new Cliente();
+		cliente2.setNome("Marcus da Silva");
+		cliente2.setEmail("marcusjava89@gmail.com");
+		cliente2.setTelefone("21999625322");
+		cliente2.setDataDeCadastro(java.sql.Date.valueOf("2024-06-10"));
+		clienteRepository.save(cliente2);
+
+		Cliente cliente3 = new Cliente();
+		cliente3.setNome("Jorge Jesus");
+		cliente3.setEmail("jorgejesus@gmail.com");
+		cliente3.setTelefone("2186450456");
+		cliente3.setDataDeCadastro(java.sql.Date.valueOf("2024-06-10"));
+		clienteRepository.save(cliente3);
+
+		List<Cliente> resposta = clienteRepository.buscaNome("joca");
+
+		assertEquals(0, resposta.size());
+
+	}
+
 }
